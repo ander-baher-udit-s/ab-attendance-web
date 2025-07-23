ce033ab92bd400b60cc6068313716c574ce0d779
ander-baher-udit-s <udit.s@anderbaher.com>
2025-07-23 18:41:06 +0530

    test_build - with ref param

--- test_build/flutter_service_worker.js
+++ test_build/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "140515e5140cb9f214e2e41f18153165
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "bc2c9f8f9f6c76181fa8497145628648",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "6ed7a25b1367f917aea2bc20b0633afd",
+"flutter_bootstrap.js": "3f31a5ef34459685453829b8c050e72c",
 "icons/Icon-192.png": "d6591cbca6faf707cccb403484fe6bab",
 "icons/Icon-512.png": "a4e76d993352ccaeea3aa867468e1208",
 "icons/Icon-maskable-192.png": "d6591cbca6faf707cccb403484fe6bab",
 "icons/Icon-maskable-512.png": "a4e76d993352ccaeea3aa867468e1208",
 "index.html": "e6b1fb5787da4045791b72e3df848acb",
 "/": "e6b1fb5787da4045791b72e3df848acb",
-"main.dart.js": "c17556e223b274b902d6a3fcb88e4c1e",
+"main.dart.js": "680e7176b05eef13b8f7a8f0051e8d21",
 "manifest.json": "b01192a20dfecef7e4a825b44753062a",
 "version.json": "d10c06c6c58200049c2a067ae00a198a"};
 // The application shell files that are downloaded before a service worker can
